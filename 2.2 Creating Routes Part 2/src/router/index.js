d79aabf3514c2c55e89dc89a8f9433215f026aeb
gcloptonchemistry <68126473+gcloptonchemistry@users.noreply.github.com>
2020-07-10 14:36:41 -0500

    Add files via upload

---
+++ 2.2 Creating Routes Part 2/src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Brazil from "../views/Brazil";
+import Hawaii from "../views/Hawaii";
+import Jamaica from "../views/Jamaica";
+import Panama from "../views/Panama";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+    {
+       path: "/brazil",
+       name: "brazil",
+       component: Brazil
+    },
+    {
+        path: "/hawaii",
+        name: "hawaii",
+        component: Hawaii
+    },
+    {
+        path: "/jamaica",
+        name: "jamaica",
+        component: Jamaica
+    },
+    {
+        path: "/panama",
+        name: "panama",
+        component: Panama
+    }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
